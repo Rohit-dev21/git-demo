9aceeaca872f3f6155f003d7397ca83efa511c7f
rohit somwanshi <rohitpro193@gmail.com>
2023-03-30 11:45:48 +0530

    rohit

---
+++ javaa/src/com/jj/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package com.jj;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		List<String > li = new ArrayList<String>();
+		li.add("rohit");
+		li.add("rohan");
+		li.add("ram");
+		
+		for(String s :li) {
+			System.out.println(s);
+
+	}
+
+}}
